fb445c465af1490f010c4aae61664fb042b67d98
Dylan Rice <dylanpatrickrice@gmail.com>
2023-03-11 17:42:56 +0000

    Create and display the board
    Also generates the computer's code

--- mastermind.rb
+++ mastermind.rb
@@ -1,19 +1,42 @@
+module Colors
+  COLORS = ["yellow", "white", "black", "brown", "orange", "red", "green", "blue"]
+end
+
 class Game
+  include Colors
+
+  @@human_wins = 0
+  @@computer_wins = 0
+
   def initialize
-    
+    @decoding_board = Array.new(12) { Array.new(4, "_") }
+    @computer_code = Computer.new
+  end
+
+  def display_board ()
+    p COLORS
+    @decoding_board.each do |row| 
+      p row
+      puts "\n"
+    end
   end
 end
 
-class Player
-  def initialize
+class Computer
+  include Colors
 
+  def initialize
+    @code = COLORS.sample(4)
   end
 end
 
-class HumanPlayer < Player
+class Human
+  include Colors
 
+  def initialize
+    @player_choice = Array.new(4)
+  end
 end
 
-class ComputerPlayer < Player
-
-end
+game = Game.new
+game.display_board
