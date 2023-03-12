e8cc0eb4ad8274d6a480ba409c59a8599aa22b5d
Dylan Rice <dylanpatrickrice@gmail.com>
2023-03-12 19:23:28 +0000

    Allow player to make a guess and display it on the decoding board

--- mastermind.rb
+++ mastermind.rb
@@ -8,17 +8,36 @@ class Game
   @@human_wins = 0
   @@computer_wins = 0
 
+  attr_accessor :game_board, :player
+
   def initialize
-    @decoding_board = Array.new(12) { Array.new(4, "_") }
-    @computer_code = Computer.new
+    @game_board = GameBoard.new
+    @computer = Computer.new
+    @player = Human.new
+  end
+
+  def check_win
+    p @computer_code == @player_guess ? true : false
   end
 
   def display_board ()
     p COLORS
-    @decoding_board.each do |row| 
-      p row
+    for i in 0..11
+      p game_board.decoding_board[i]
+      puts game_board.feedback_board[i]
       puts "\n"
     end
+  end
+
+  def get_guess
+    gets.chomp.split(" ")
+  end
+
+  def take_turn
+    player.player_choice = get_guess
+    game_board.decoding_board[player.turn_number] = player.player_choice
+    player.turn_number += 1
+    display_board
   end
 end
 
@@ -33,10 +52,23 @@ end
 class Human
   include Colors
 
+  attr_accessor :player_choice, :turn_number
+
   def initialize
-    @player_choice = Array.new(4)
+    @player_choice = []
+    @turn_number = 0
+  end
+end
+
+class GameBoard
+  attr_accessor :decoding_board, :feedback_board
+
+  def initialize
+    @decoding_board = Array.new(12) { Array.new(4, "_") }
+    @feedback_board = Array.new(12, "")
   end
 end
 
 game = Game.new
-game.display_board
+game.take_turn
+game.take_turn
